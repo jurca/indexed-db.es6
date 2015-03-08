932f78e95961fbc1f3a5d78c4899ad2d5c829bb9
Martin Jurča <mjurca@centrum.cz>
2015-03-08 21:28:53 +0100

    Prepared the dev environment

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON("package.json"),
+
+    traceur: {
+      options: {
+        modules: "amd",
+        moduleNames: false,
+      },
+      build: {
+        files: [{
+          expand: true,
+          cwd: "src",
+          src: "**/*.js",
+          dest: "compiled"
+        }]
+      }
+    },
+
+    watch: {
+      options: {
+        atBegin: true,
+        spawn: false
+      },
+      scripts: {
+        files: ["src/**/*.js"],
+        tasks: ["traceur"]
+      }
+    }
+  })
+
+  grunt.loadNpmTasks("grunt-traceur")
+  grunt.loadNpmTasks("grunt-contrib-watch")
+
+  grunt.registerTask("default", ["traceur"])
+}
