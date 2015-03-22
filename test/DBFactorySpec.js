7bd75b61a7149cd7a2fd71759ec0cb5e902c1a37
Martin Jurča <mjurca@centrum.cz>
2015-03-22 12:56:59 +0100

    first actual test

--- test/DBFactorySpec.js
+++ test/DBFactorySpec.js
@@ -1,12 +1,41 @@
+
 import DBFactory from "../compiled/DBFactory"
+import DatabaseSchema from "../compiled/schema/DatabaseSchema"
+import ObjectStoreSchema from "../compiled/schema/ObjectStoreSchema"
 
 describe("DBFactory", () => {
 
-  beforeEach(() => {
-    // TODO
+  const DB_NAME = "testing database"
+
+  afterEach((done) => {
+    let request = indexedDB.deleteDatabase(DB_NAME)
+    request.onsuccess = () => done()
+    request.onerror = () => done(request.error)
   })
 
-  it("should run karma tests", () => {
-    // TODO
+  it("should connect to an existing database", (done) => {
+    let request = indexedDB.open(DB_NAME, 1)
+    request.onerror = () => done(request.error)
+    request.onblocked = () => done(new Error("failed to create the database"))
+    request.onupgradeneeded = () => {
+      let database = request.result
+      database.onerror = done
+      database.createObjectStore("testingStore")
+    }
+
+    request.onsuccess = () => {
+      let database = request.result
+      database.close()
+
+      DBFactory.open(DB_NAME,
+        new DatabaseSchema(1,
+          new ObjectStoreSchema("testingStore", null, false)
+        )
+      ).then((database) => {
+        database.close()
+      }).catch((error) => done(error))
+
+      done()
+    }
   })
 })
