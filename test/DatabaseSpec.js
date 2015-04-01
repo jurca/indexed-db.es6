3e5bde7ef1e320cdc1c7a02943e90cae12cd7bdc
Martin Jurča <mjurca@centrum.cz>
2015-04-01 20:40:53 +0200

    first test of the Database class

---
+++ test/DatabaseSpec.js
@@ -0,0 +1,58 @@
+
+import DBFactory from "../compiled/DBFactory"
+import DatabaseSchema from "../compiled/schema/DatabaseSchema"
+import ObjectStoreSchema from "../compiled/schema/ObjectStoreSchema"
+import UpgradedDatabaseSchema from "../compiled/schema/UpgradedDatabaseSchema"
+
+describe("Database", () => {
+  
+  const DB_NAME = "testing database"
+  
+  let database
+  
+  beforeEach((done) => {
+    DBFactory.open(DB_NAME,
+      new DatabaseSchema(1,
+        new ObjectStoreSchema("fooBar")
+      )
+    ).then((databaseInstance) => {
+      database = databaseInstance
+      done()
+    }).catch((error) => fail(error))
+  })
+  
+  afterEach((done) => {
+    database.close()
+    
+    let request = indexedDB.deleteDatabase(DB_NAME)
+    request.onsuccess = () => done()
+    request.onerror = () => fail(request.error)
+  })
+  
+  it("allows version change request to be listened for", (done) => {
+    var listenerExecuted = false
+    
+    database.addVersionChangeListener((newVersion) => {
+      expect(newVersion).toBe(77)
+      listenerExecuted = true
+      
+      database.close()
+    })
+    
+    DBFactory.open(DB_NAME,
+      new DatabaseSchema(1,
+        new ObjectStoreSchema("fooBar")
+      ),
+      new UpgradedDatabaseSchema(77, [], [
+        new ObjectStoreSchema("fooBar2")
+      ])
+    ).then((databaseInstance) => {
+      databaseInstance.close()
+      
+      expect(listenerExecuted).toBeTruthy()
+      
+      done()
+    }).catch((error) => fail(error))
+  })
+  
+})
