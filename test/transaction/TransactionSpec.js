a343b4f7ab3c2f50d2ab9eb6ac94e2cdfb57a3db
Martin Jurča <mjurca@centrum.cz>
2015-04-04 15:54:02 +0200

    added tests for the Transaction class

---
+++ test/transaction/TransactionSpec.js
@@ -0,0 +1,42 @@
+
+import DBFactory from "../../compiled/DBFactory"
+import DatabaseSchema from "../../compiled/schema/DatabaseSchema"
+import ObjectStoreSchema from "../../compiled/schema/ObjectStoreSchema"
+
+describe("Transaction", () => {
+  const DB_NAME = "testing database"
+  const OBJECT_STORE_NAME = "fooBar"
+  
+  let database
+  let transaction
+  
+  beforeEach((done) => {
+    DBFactory.open(DB_NAME,
+      new DatabaseSchema(1,
+        new ObjectStoreSchema(OBJECT_STORE_NAME)
+      )
+    ).then((databaseInstance) => {
+      database = databaseInstance
+      transaction = database.startTransaction(OBJECT_STORE_NAME)
+      done()
+    }).catch((error) => fail(error))
+  })
+  
+  afterEach((done) => {
+    database.close()
+    
+    let request = indexedDB.deleteDatabase(DB_NAME)
+    request.onsuccess = () => done()
+    request.onerror = () => fail(request.error)
+  })
+  
+  it("should provide read-write access to object stores", (done) => {
+    let objectStore = transaction.getObjectStore(OBJECT_STORE_NAME)
+    objectStore.add("Foooooo... bar!", 11).then(() => {
+      return transaction.completionPromise
+    }).then(() => {
+      done()
+    }).catch((error) => fail(error))
+  })
+  
+})
