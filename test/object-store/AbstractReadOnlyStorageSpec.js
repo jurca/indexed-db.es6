e707712ed608650003967af5fe5a90252a169756
Martin Jurča <mjurca@centrum.cz>
2015-04-06 12:17:20 +0200

    finished test for AbstractReadOnlyStorageSpec.forEach

--- test/object-store/AbstractReadOnlyStorageSpec.js
+++ test/object-store/AbstractReadOnlyStorageSpec.js
@@ -76,6 +76,56 @@ describe("AbstractReadOnlyStorage", () => {
     }).then((recordCount) => {
       expect(recordCount).toBe(3)
       expect(callbackExecuted).toBeTruthy()
+      
+      lastId = 1000
+      return objectStore.forEach(undefined, CursorDirection.PREVIOUS, (r) => {
+        expect(r.id).toBeLessThan(lastId)
+        lastId = r.id
+      })
+    }).then((count) => {
+      expect(count).toBe(3)
+      
+      return objectStore.forEach(14, CursorDirection.NEXT, (record) => {
+        expect(record.id).toBe(14)
+      })
+    }).then((recordCount) => {
+      expect(recordCount).toBe(1)
+      
+      let NEXT = CursorDirection.NEXT
+      return objectStore.forEach(KeyRange.bound(10, 15), NEXT, (record) => {
+        expect(record.id).toBeLessThan(16)
+      })
+    }).then((recordCount) => {
+      expect(recordCount).toBe(2)
+      
+      return objectStore.forEach({
+        id: KeyRange.bound(10, 15),
+        name: "Adam"
+      }, CursorDirection.NEXT, (record) => {
+        expect(record.name).toBe("Adam")
+      })
+    }).then((recordCount) => {
+      expect(recordCount).toBe(1)
+      
+      return objectStore.forEach({
+        id: 14
+      }, CursorDirection.NEXT, (record) => {
+        expect(record.name).toBe("Adam")
+      })
+    }).then((recordCount) => {
+      expect(recordCount).toBe(1)
+      
+      return objectStore.forEach((record, id, key) => {
+        expect(id).toBe(key)
+        expect(id).toBeGreaterThan(10)
+        
+        return record.name === "Adam"
+      }, CursorDirection.NEXT, (record) => {
+        expect(record.name).toBe("Adam")
+      })
+    }).then((recordCount) => {
+      expect(recordCount).toBe(1)
+      
       done()
     }).catch(error => fail(error.stack || error.message))
   })
