58058b28eb6b18403841b88eff7c123ae14b807f
Martin Jurča <mjurca@centrum.cz>
2015-05-20 20:10:31 +0200

    added basic tests for the ReadOnlyObjectStore.query() method

--- test/object-store/ReadOnlyObjectStoreSpec.js
+++ test/object-store/ReadOnlyObjectStoreSpec.js
@@ -11,6 +11,7 @@ describe("ReadOnlyObjectStore", () => {
   const DB_NAME = "testing database"
   const OBJECT_STORE_NAME = "fooBar"
   const OBJECT_STORE_NAME2 = "fooBar2"
+  const OBJECT_STORE_NAME3 = "fooBar3"
   
   let database
   let transaction
@@ -25,6 +26,11 @@ describe("ReadOnlyObjectStore", () => {
         new ObjectStoreSchema(OBJECT_STORE_NAME2, ["id1", "id2"], false,
           new IndexSchema("someIndex", "keyField"),
           new IndexSchema("otherIndex", ["keyField", "otherKey"])
+        ),
+        new ObjectStoreSchema(OBJECT_STORE_NAME3, "id", true,
+          new IndexSchema("index1", "id"),
+          new IndexSchema("index2", "category", false, true),
+          new IndexSchema("index3", "age")
         )
       )
     ).then((databaseInstance) => {
@@ -91,6 +97,119 @@ describe("ReadOnlyObjectStore", () => {
   it("should return the same index when call repeatedly", () => {
     let index = objectStore.getIndex("someIndex")
     expect(objectStore.getIndex("someIndex")).toEqual(index)
+  })
+  
+  describe("query", () => {
+    beforeEach(() => {
+      transaction = database.startTransaction(OBJECT_STORE_NAME3)
+      objectStore = transaction.getObjectStore(OBJECT_STORE_NAME3)
+      
+      objectStore.add({
+        category: 1,
+        age: 10,
+        bio: {
+          cat: 1
+        }
+      })
+      objectStore.add({
+        category: 2,
+        age: 8,
+        bio: {
+          cat: 2
+        }
+      })
+      objectStore.add({
+        category: [2, 3],
+        age: 11,
+        bio: {
+          cat: 2
+        }
+      })
+      objectStore.add({
+        category: 4,
+        age: 12,
+        bio: {
+          cat: 4
+        }
+      })
+    })
+    
+    it("should fetch all records", (done) => {
+      objectStore.query().then((records) => {
+        expect(recordsToIds(records)).toEqual([1, 2, 3, 4])
+        done()
+      })
+    })
+    
+    it("should allow filtering", (done) => {
+      objectStore.query(2).then((records) => {
+        expect(recordsToIds(records)).toEqual([2])
+        
+        return objectStore.query(KeyRange.bound(2, 3))
+      }).then((records) => {
+        expect(recordsToIds(records)).toEqual([2, 3])
+        
+        return objectStore.query({ id: 3 })
+      }).then((records) => {
+        expect(recordsToIds(records)).toEqual([3])
+        
+        return objectStore.query((record, primaryKey, key) => {
+          return primaryKey > 2
+        })
+      }).then((records) => {
+        expect(recordsToIds(records)).toEqual([3, 4])
+        
+        return objectStore.query({ category: [2, 3] })
+      }).then((records) => {
+        expect(recordsToIds(records)).toEqual([3])
+        
+        return objectStore.query({ age: KeyRange.bound(11, 12) })
+      }).then((records) => {
+        done()
+      }).catch((error) => {
+        fail(error)
+        done()
+      })
+    })
+    
+    it("should allow sorting", (done) => {
+      objectStore.query(null, "neXt").then((records) => {
+        expect(recordsToIds(records)).toEqual([1, 2, 3, 4])
+        
+        return objectStore.query(null, CursorDirection.PREVIOUS)
+      }).then((records) => {
+        expect(recordsToIds(records)).toEqual([4, 3, 2, 1])
+        
+        return objectStore.query(null, null)
+      }).then((records) => {
+        expect(recordsToIds(records)).toEqual([1, 2, 3, 4])
+        
+        return objectStore.query(null, "id")
+      }).then((records) => {
+        expect(recordsToIds(records)).toEqual([1, 2, 3, 4])
+        
+        return objectStore.query(null, "!id")
+      }).then((records) => {
+        expect(recordsToIds(records)).toEqual([4, 3, 2, 1])
+        
+        return objectStore.query(null, ["!bio.cat", "id"])
+      }).then((records) => {
+        expect(recordsToIds(records)).toEqual([4, 2, 3, 1])
+        
+        return objectStore.query(null, (r1, r2) => r1.id - r2.id)
+      }).then((records) => {
+        expect(recordsToIds(records)).toEqual([1, 2, 3, 4])
+        
+        done()
+      }).catch((error) => {
+        fail(error)
+        done()
+      })
+    })
+    
+    function recordsToIds(records) {
+      return records.map(record => record.id)
+    }
   })
   
 })
